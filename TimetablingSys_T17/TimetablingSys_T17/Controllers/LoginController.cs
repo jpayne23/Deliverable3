5a0897b6ea64ea0c87b1b841e7ba8a942169cdfa
amaise <amaise@live.com>
2015-03-13 11:42:45 +0000

    ALMOST THERE.

---
+++ TimetablingSys_T17/TimetablingSys_T17/Controllers/LoginController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimetablingSys_T17.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private bool validateDetails(string deptIn, string password)
+        {
+
+            bool returnVal = false;
+
+            using (var db = new TimetableDbEntities())
+            {
+
+                var user = from userDB in db.User1 from dInfoDB in db.DeptInfoes where userDB.deptID == dInfoDB.deptID && dInfoDB.deptName == deptIn select userDB.email;
+
+                // search for email, i.e. username, email = uniq to display. Are doing usernames or just depts?
+
+                if (user.FirstOrDefault() != null)
+                {
+
+                    var inputPass = from userDB in db.User1 from dInfoDB in db.DeptInfoes where userDB.deptID == dInfoDB.deptID && dInfoDB.deptName == deptIn select userDB.password;
+
+                    if (inputPass.FirstOrDefault() == password)
+                    {
+                        returnVal = true;
+                    }
+                }
+            }
+
+
+
+            return returnVal;
+
+        }
+
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet] // Used when we initially go into login
+        public ActionResult Login()
+        {
+
+            return View();
+
+        }
+
+        [HttpPost] // validation - rtn deptlogin from input, validate
+        public ActionResult Login(Models.LoginModel deptLogin) {
+
+            if (ModelState.IsValid)
+            {
+
+                if (validateDetails(deptLogin.deptIn, deptLogin.password))
+                {
+
+                    return RedirectToAction("Temp", "Index");
+                }
+                else
+                {
+
+                }
+    
+
+            }
+            return View();
+        }
+	}
+}
---
+++ TimetablingSys_T17/TimetablingSys_T17/Models/LoginModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TimetablingSys_T17.Models
+{
+    public class LoginModel
+    {
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name="Department ID (Case sensitive for now): ")]
+        public string deptIn { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength=4)]
+        [Display(Name="Password: ")]
+        public string password { get; set; }
+
+        // Salt and pepper later
+        // public string PasswordSalt { get; set; }
+        
+    }
+}
