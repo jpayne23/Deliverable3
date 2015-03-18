8d3d7f5b00ccbdbc46c5aeac6131730e98712491
jpayne23 <payne_500@hotmail.com>
2015-03-18 19:19:51 +0000

    Added data entry for room creation
    No functionality just the template

---
+++ TimetableSys_T17/TimetableSys_T17/Models/CreateRoomModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimetableSys_T17.Models
+{
+    public class CreateRoomModel
+    {
+        public string roomName { get; set; }
+        public int capacity { get; set; }
+        public string buildingCode { get; set; }
+        public Boolean lab { get; set; }
+    }
+}
