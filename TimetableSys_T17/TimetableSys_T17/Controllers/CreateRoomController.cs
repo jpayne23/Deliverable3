58b63d2c118e7ebda7a2360bf56c16c061c55185
jpayne23 <payne_500@hotmail.com>
2015-03-22 22:37:26 +0000

    added controller

--- TimetableSys_T17/TimetableSys_T17/Controllers/CreateRoomController.cs
+++ TimetableSys_T17/TimetableSys_T17/Controllers/CreateRoomController.cs
@@ -8,11 +8,42 @@ namespace TimetableSys_T17.Controllers
 {
     public class CreateRoomController : Controller
     {
+
+        private int isLab(bool lab)
+        {
+
+            int labValue = 0;
+
+            if (lab == true)
+            {
+                labValue = 1;
+            }
+            else
+            {
+                labValue = 0;
+            }
+
+            return labValue;
+        }
+
+
         //
         // GET: /CreateRoom/
         public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(Models.CreateRoomModel room)
         {
+            if (isLab(room.lab) == 1)
+            {
+                ViewData["Message"] = "Success";
+            }
+            else 
+            {
+                ViewData["Message"] = "Failure";
+            }
+
             return View();
         }
+
 	}
 }
