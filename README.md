38a6745d5358d3bbe6c9404d4105c4d5f77de945
jpayne23 <payne_500@hotmail.com>
2015-03-24 20:58:48 +0000

    attempt to fix master

---
+++ README.md
@@ -0,0 +1,2 @@
+# Deliverable3
+Team Projects deliverable 3 for Team 17
---
+++ TimetableSys_T17/TimetableSys_T17/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimetableSys_T17.Startup))]
+namespace TimetableSys_T17
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
