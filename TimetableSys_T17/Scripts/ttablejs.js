3282c937494f9349e55fba65d514ede954cf5dd6
amaise <amaise@live.com>
2015-04-19 14:20:52 +0100

    Functioning Ajax - Milestone
    jQuery, jQuery-UI and Modernizer updated. Added own JS file for custom
    functions.

---
+++ TimetableSys_T17/Scripts/ttablejs.js
@@ -0,0 +1,48 @@
+﻿$(function () {
+
+    var test = function () {
+
+        $host = $(this);
+        var target = $("#" + $host.attr("data-ttablejs-target")); // Find target in DOM.
+        var input = $host.val();
+
+
+      $.ajax({
+
+          url: "bark",
+          type: "GET",
+          data: {
+
+              input: input
+          },
+          contentType: "application/json",
+          success: function (data) {
+
+              $("#test2").autocomplete({
+
+                  source: data.parkName
+              });
+            
+          }
+
+      });
+    }
+
+
+    $("input[data-ttablejs-autocomplete2]").keyup(test);
+    
+
+    
+
+    /*
+
+        jQuery selector, find all the forms with data attribute
+        evaluted to true.
+
+        Invokes function ajaxFormSubmit.
+
+
+    */
+
+
+});
